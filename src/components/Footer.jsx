6084aa5e3d2b2b749546557e045bad5b1b883d59
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-03 14:57:13 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ import { Email, Facebook, Instagram, Linkedin, Phone } from './Icons'
 const Footer = () => {
     return (
         <>
-            <div className=' bg-[#000000] py-[20px] relative overflow-hidden' >
+            <div className=' bg-[#000000] pb-7 md:py-10 lg:py-[20px] relative overflow-hidden' >
                 <div className=' max-w-[1160px] container mx-auto px-3 '>
-                    <div className=' flex flex-wrap  flex-row -mx-3 pt-[50px]'>
+                    <div className=' flex flex-wrap  flex-row -mx-3 pt-3 sm:pt-5 md:pt-[50px]'>
                         <div className='w-full xl:w-1/3 px-3'>
                             <img src={PageLogo} alt="Footerlogo" />
                             <p className='font-["inter"] font-medium text-[16px]  leading-6  max-w-[369px] text-[#CCCCCC] pt-[16px]'>En Exclusive Games somos un equipo apasionado de personas dedicadas al desarrollo de multiplataformas para juegos de azar</p>
-                            <div className=" flex gap-5 pt-6">
+                            <div className=" flex gap-5 pt-3 sm:pt-6">
                                 <a href="https://www.instagram.com/" target="_blank">
                                     <span><Instagram /></span>
                                 </a>
@@ -24,24 +24,24 @@ const Footer = () => {
                                 </a>
                             </div>
                         </div>
-                        <div className='w-full xl:w-2/3 px-3 pt-5 xl:pt-0'>
+                        <div className='w-full xl:w-2/3 px-3 pt-3 sm:pt-5 xl:pt-0'>
                             <div className=' flex flex-wrap flex-row -mx-3'>
                                 <div className='sm:w-1/2  w-full lg:w-1/3 px-3'>
                                     <ul>
                                         <li className=' font-["inter"] font-bold text-[16px]  leading-6  text-[#FFFFFF] pt-[10px]'>Menú principal</li>
-                                        <li className=' pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Hogar</a></li>
-                                        <li className=' pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Misión</a></li>
-                                        <li className=' pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Tragamonedas</a></li>
-                                        <li className=' pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Por qué elegirnos</a></li>
-                                        <li className=' pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Ofertas</a></li>
+                                        <li className=' pt-2 sm:pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Hogar</a></li>
+                                        <li className=' pt-2 sm:pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Misión</a></li>
+                                        <li className=' pt-2 sm:pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Tragamonedas</a></li>
+                                        <li className=' pt-2 sm:pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Por qué elegirnos</a></li>
+                                        <li className=' pt-2 sm:pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Ofertas</a></li>
                                     </ul>
                                 </div>
                                 <div className='sm:w-1/2  w-full lg:w-1/3 px-3 pt-4 sm:pt-0'>
                                     <ul>
                                         <li className=' font-["inter"] font-bold text-[16px]  leading-6  text-[#FFFFFF] pt-[10px]'>Atención al cliente</li>
-                                        <li className=' pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>¿Necesitas ayuda?</a></li>
-                                        <li className=' pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>política de privacidad</a></li>
-                                        <li className=' pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Términos de servicios</a></li>
+                                        <li className=' pt-2 sm:pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>¿Necesitas ayuda?</a></li>
+                                        <li className=' pt-2 sm:pt-4'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>política de privacidad</a></li>
+                                        <li className=' pt-2 sm:pt-3'><a href="#" className=' font-["inter"] font-medium text-[16px] text-[#CCCCCC]  leading-6'>Términos de servicios</a></li>
                                     </ul>
                                 </div>
                                 <div className='w-full lg:w-1/3 px-3 pt-4 lg:pt-0'>
