9aa5237ee64b60bf2dd4734275e328bd2ea06f17
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-03 14:52:52 +0530

    Preguntas section modified

--- src/components/Preguntas.jsx
+++ src/components/Preguntas.jsx
@@ -18,39 +18,38 @@ function Icon({ id, open }) {
 
 const Preguntas = () => {
     const [open, setOpen] = React.useState(1);
-
     const handleOpen = (value) => setOpen(open === value ? 0 : value);
     return (
         <div className='max-w-[970px] mx-auto px-3'>
-            <div className='py-[150px]'>
-                <h2 className=' text-5xl font-anton leading-[120%] font-normal text-center pb-[60px] text-white'>Preguntas más frecuentes</h2>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5" open={open === 1} icon={<Icon id={1} open={open} />}                >
-                    <AccordionHeader onClick={() => handleOpen(1)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%] " >¿Cuáles son las ventajas de elegir Juegos Exclusivos para mis plataformas de juegos?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+            <div className=' py-7 sm:py-10 md:py-20 lg:py-[150px]'>
+                <h2 className=' text-[23px] sm:text-2xl md:text-3xl lg:text-5xl font-anton leading-[120%] font-normal text-center pb-4 sm:pb-7 md:pb-[60px] text-white'>Preguntas más frecuentes</h2>
+                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-3 md:px-5" open={open === 1} icon={<Icon id={1} open={open} />}                >
+                    <AccordionHeader onClick={() => handleOpen(1)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%] " >¿Cuáles son las ventajas de elegir Juegos Exclusivos para mis plataformas de juegos?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5  overflow-hidden" open={open === 2} icon={<Icon id={2} open={open} />} >
-                    <AccordionHeader onClick={() => handleOpen(2)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%]"> ¿Cómo garantizan la seguridad de las operaciones en sus juegos?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-3 md:px-5  overflow-hidden" open={open === 2} icon={<Icon id={2} open={open} />} >
+                    <AccordionHeader onClick={() => handleOpen(2)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%]"> ¿Cómo garantizan la seguridad de las operaciones en sus juegos?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5 " open={open === 3} icon={<Icon id={3} open={open} />}>
-                    <AccordionHeader onClick={() => handleOpen(3)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%]" > ¿Cuáles son las opciones de juego disponibles en las versiones Silver, Luxury y Platinum?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-3 md:px-5 " open={open === 3} icon={<Icon id={3} open={open} />}>
+                    <AccordionHeader onClick={() => handleOpen(3)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%]" > ¿Cuáles son las opciones de juego disponibles en las versiones Silver, Luxury y Platinum?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5 " open={open === 4} icon={<Icon id={4} open={open} />}>
-                    <AccordionHeader onClick={() => handleOpen(4)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%]">¿Cuánto tiempo lleva crear una plataforma con Juegos Exclusivos?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-3 md:px-5 " open={open === 4} icon={<Icon id={4} open={open} />}>
+                    <AccordionHeader onClick={() => handleOpen(4)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%]">¿Cuánto tiempo lleva crear una plataforma con Juegos Exclusivos?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5 " open={open === 5} icon={<Icon id={5} open={open} />}>
-                    <AccordionHeader onClick={() => handleOpen(5)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%]">¿Qué métodos de pago aceptan?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-3 md:px-5 " open={open === 5} icon={<Icon id={5} open={open} />}>
+                    <AccordionHeader onClick={() => handleOpen(5)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%]">¿Qué métodos de pago aceptan?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
-                <Accordion className=" border-[#80898D] border mb-4 rounded-[10px] py-4 px-5 " open={open === 6} icon={<Icon id={6} open={open} />}>
-                    <AccordionHeader onClick={() => handleOpen(6)} className="font-anton text-[20px] sm:text-[16px] md:text-[20px] py-0 text-start font-normal text-white leading-[130%]">¿Puedo probar sus juegos antes de comprometerme?</AccordionHeader>
-                    <AccordionBody className="font-inter text-base md:text-[16px] font-medium leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
+                <Accordion className=" border-[#80898D] border rounded-[10px] py-4 px-3 md:px-5 " open={open === 6} icon={<Icon id={6} open={open} />}>
+                    <AccordionHeader onClick={() => handleOpen(6)} className="font-anton text-[13px] md:text-[20px] sm:text-[16px] py-0 text-start font-normal text-white leading-[130%]">¿Puedo probar sus juegos antes de comprometerme?</AccordionHeader>
+                    <AccordionBody className="font-inter text-xs md:text-[16px] font-medium leading-[130%] md:leading-[160%] text-[#CCD0D1] max-w-[880px] pt-2 pb-0 ">En Exclusive Games, ofrecemos experiencias únicas y personalizadas, respaldadas por más de 15 años de dedicación al desarrollo de multiplataformas para juegos de azar.</AccordionBody>
                 </Accordion>
             </div>
-            <div className='w-full rounded-2xl bg-preguntas py-[100px] mb-[150px] border-[#829094] border'>
-                <h2 className=' text-[32px] font-anton font-normal leading-[130%] text-white text-center pb-10'>Lanza tu propia plataforma en sólo 2 semanas</h2>
+            <div className='w-full rounded-2xl bg-preguntas py-4 sm:py-9 md:py-12 lg:py-[100px] mb-7 sm:mb-10 md:mb-20 lg:mb-[150px] border-[#829094] border'>
+                <h2 className=' text-[25px] md:text-[32px] font-anton font-normal leading-[130%] text-white text-center pb-10'>Lanza tu propia plataforma en sólo 2 semanas</h2>
                 <div className="flex justify-center">
                     <div className='relative'>
                         <SecondaryBtn text='Leer más' />
