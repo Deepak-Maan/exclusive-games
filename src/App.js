d6ff3bf06ef9c6ea68aa7bd9f6371d0a7b5cbaaf
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-05 10:14:51 +0530

    added meta tags

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Backtotop from "./components/BackToTop";
 import Cashino from "./components/Cashino";
@@ -9,32 +10,48 @@ import Mision from "./components/Mision";
 import Nav from "./components/Nav";
 import Nuestros from "./components/Nuestros";
 import Preguntas from "./components/Preguntas";
+import Preloader from "./components/Preloader";
 import Spin from "./components/Spin";
 import TripleSeven from "./components/TripleSeven";
 
 function App() {
+  const [data, setdata] = useState(true);
+  useEffect(() => {
+    setdata(true);
+    setTimeout(() => {
+      setdata(false);
+    }, 3000);
+  }, []);
   return (
-    <div className="bg-[#00141B]">
-      <div className=" bg-hero">
-        <Nav />
-        <Hero />
-      </div>
-      <Mision />
-      <div className='max-md:bg-white md:bg-[url("../src/assets/images/bg-nuestros.png")] bg-no-repeat  bg-size'>
-        <Nuestros />
-      </div>
-      <div className="bg-[url('./assets/images/jackpot-bg.png')] bg-no-repeat bg-size">
-        <Jackpot />
-      </div>
-      <div className="max-md:bg-white md:bg-[url('./assets/images/casino-Bg.png')] bg-no-repeat bg-size">
-        <Cashino />
-      </div>
-      <TripleSeven />
-      <Spin />
-      <Ellos />
-      <Preguntas />
-      <Footer />
-      <Backtotop />
+    <div>
+      {data ? (
+        <div>
+          <Preloader />
+        </div>
+      ) : (
+        <div className="bg-[#00141B]">
+          <div className=" bg-hero">
+            <Nav />
+            <Hero />
+          </div>
+          <Mision />
+          <div className='max-md:bg-white md:bg-[url("../src/assets/images/bg-nuestros.png")] bg-no-repeat  bg-size'>
+            <Nuestros />
+          </div>
+          <div className="bg-[url('./assets/images/jackpot-bg.png')] bg-no-repeat bg-size">
+            <Jackpot />
+          </div>
+          <div className="max-md:bg-white md:bg-[url('./assets/images/casino-Bg.png')] bg-no-repeat bg-size">
+            <Cashino />
+          </div>
+          <TripleSeven />
+          <Spin />
+          <Ellos />
+          <Preguntas />
+          <Footer />
+          <Backtotop />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/SecondaryBtn.jsx
+++ src/components/SecondaryBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SecondaryBtn = (props) => {
     return (
         <div>
-            <button className=' bg-blue-307 py-[13px] px-[46px] text-black font-Inter font-bold text-base leading-6 rounded-full duration-300 border border-transparent'>{props.text}</button>
+            <button className=' bg-blue-307 relative z-[3] py-[13px] px-[46px] hover:scale-[1.1] text-black font-Inter font-bold text-base leading-6 rounded-full duration-300 border border-transparent'>{props.text}</button>
         </div>
     )
 }
